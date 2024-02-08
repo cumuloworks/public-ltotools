5460caa724665266e18c9ee8ddf19974b3058719
Tomoya Eguchi <mail@cumulo.works>
2024-02-08 21:34:56 +0900

    render downloader

--- scripts/RNDR_downloader.py
+++ scripts/RNDR_downloader.py
@@ -1,24 +1,44 @@
 import tkinter as tk
+from tkinter import filedialog
 import os
 import urllib.parse
 import subprocess
-from tkinter import filedialog
-from concurrent.futures import ThreadPoolExecutor
+import datetime
+import time
+from tqdm import tqdm
+import threading
 
-def download_files(download_dir, urls):
+def monitor_download_progress(directory, expected_count, text_file_name, interval=0.2):
+    dir_name = os.path.basename(directory)
+    # ディレクトリ名とテキストファイル名をプログレスバーの説明として使用
+    pbar = tqdm(total=expected_count, desc=f"{text_file_name} -> {dir_name}", unit="file", leave=False)
+    while pbar.n < expected_count:
+        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and not f.endswith('.aria2')]
+        pbar.update(len(files) - pbar.n)
+        time.sleep(interval)
+    pbar.close()
+
+
+def download_files(download_dir, urls, text_file_name):  # 引数にtext_file_nameを追加
     download_list_file = os.path.join(download_dir, "download_list.txt")
     with open(download_list_file, 'w') as f:
         for url in urls:
             f.write(url + '\n')
 
-    command = f'aria2c -i "{download_list_file}" -d "{download_dir}" -x 16 -j 32 -c'
-    subprocess.run(command, shell=True)
+    command = f'aria2c -i "{download_list_file}" -d "{download_dir}" -x 16 -j 32 -c -q'
+    subprocess.Popen(command, shell=True)
+
+    monitor_thread = threading.Thread(target=monitor_download_progress, args=(download_dir, len(urls), text_file_name))
+    monitor_thread.start()
+    monitor_thread.join()
 
     os.remove(download_list_file)
+
 
 def process_url_list(file_path):
     file_dir = os.path.dirname(file_path)
     file_name = os.path.splitext(os.path.basename(file_path))[0]
+    text_file_name = os.path.basename(file_path)  # テキストファイルの完全な名前
 
     downloads = {}
 
@@ -47,16 +67,17 @@ def process_url_list(file_path):
             downloads[download_dir] = []
         downloads[download_dir].append(line.strip())
 
-    # 各ダウンロードディレクトリの処理を並列実行
-    with ThreadPoolExecutor() as executor:
-        for dir, urls in downloads.items():
-            executor.submit(download_files, dir, urls)
+    for dir, urls in downloads.items():
+        download_files(dir, urls, text_file_name)  
 
-root = tk.Tk()
-root.withdraw()
+def main():
+    root = tk.Tk()
+    root.withdraw()  # tkinterのメインウィンドウを表示しない
 
-file_paths = filedialog.askopenfilenames(filetypes=[("テキストファイル", "*.txt")])
+    file_paths = filedialog.askopenfilenames(title="ダウンロードするURLリストを含むテキストファイルを選択", filetypes=[("テキストファイル", "*.txt")])
 
-# 各テキストファイルの処理を並列実行
-with ThreadPoolExecutor() as executor:
-    executor.map(process_url_list, file_paths)
+    for file_path in file_paths:
+        process_url_list(file_path)
+
+if __name__ == "__main__":
+    main()
